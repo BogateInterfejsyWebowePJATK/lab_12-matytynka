cb7f646c6a8db0fac7543ef6011f7de8265dbdcc
Martyna <matytynka@o2.pl>
2021-01-18 16:30:23 +0100

    zad 2 added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/calculator';
+import Table from './components/table';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App" >
+
+          <div className="Calculator" style={{border: "1px solid black"}}>
+              <p>
+                  Zadanie 1:
+              </p>
+            <Calculator/>
+          </div>
+          <div className="Table" style={{border: "1px solid black"}}>
+              <p>
+                  Zadanie 2:
+              </p>
+              <Table/>
+          </div>
+      </div>
   );
 }
 
