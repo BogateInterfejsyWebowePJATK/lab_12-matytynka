fd2001d94bfa87c7fdce23549aabff83eab51558
Martyna <matytynka@o2.pl>
2021-01-20 13:23:54 +0100

    zad 3 added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import './App.css';
 import Calculator from './components/calculator';
 import Table from './components/table';
+import Login from './components/login';
 
 function App() {
   return (
       <div className="App" >
 
-          <div className="Calculator" style={{border: "1px solid black"}}>
+          <div className="Calculator" id="zadanie">
               <p>
                   Zadanie 1:
               </p>
             <Calculator/>
           </div>
-          <div className="Table" style={{border: "1px solid black"}}>
+          <div className="Table" id="zadanie">
               <p>
                   Zadanie 2:
               </p>
               <Table/>
+          </div>
+          <div className="Login" id="zadanie">
+          <p>
+              Zadanie 3:
+          </p>
+              <Login/>
           </div>
       </div>
   );
---
+++ src/components/login.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import userData from "../data/userdata.json"
+
+export default function Login({onLogin = f => f}) {
+
+    const [data, setData] = useState({
+        email: "",
+        pass: ""
+    });
+
+    const [error, setError] = useState("");
+
+    const [logged, setLogged] = useState(false);
+
+    function validateEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    function validate() {
+        let error = "";
+
+        if(!validateEmail(data["email"])) {
+            error = "To nie jest adres e-mail";
+        }
+        if (error === ""){
+            for(const user of userData) {
+                if (data["email"] === user.email && data["pass"] === user.password) {
+                    setError("");
+                    return user;
+                }
+            }
+            error = "Nieprawidłowy e-mail lub hasło";
+        }
+        setError(error);
+        return false;
+    }
+
+    function submit(e) {
+        e.preventDefault();
+        let user = validate();
+        if(user !== false) {
+            setLogged(true);
+            onLogin(user);
+        }
+    }
+
+    return (
+        <form onSubmit={submit}>
+            <label>E-mail:</label><br/>
+            <input value={data.email} type="text" required onChange={
+            (e) => {setData({email: e.target.value, pass: data.pass})}
+        }/><br/>
+            <label>Hasło:</label><br/>
+            <input value={data.pass} type="password" required onChange={
+            (e) => {setData({email: data.email, pass: e.target.value})}
+        }/><br/>
+            <input type="submit" value="Zaloguj się"/>
+            {logged && <p>Pomyślnie zalogowano!</p>}
+            <p>{error}</p>
+        </form>
+    )
+}
